4c8d53e356aaaf26780bb6aedd36d45ed226d4dc
mczgeogis-pixel <sarahgeogis@gmail.com>
2026-07-20 15:53:15 -0300

    Delete lib directory

--- lib/supabase-browser.ts
+++
@@ -1,7 +0,0 @@
-import { createBrowserClient } from "@supabase/ssr";
-
-export function createBrowserSupabase() {
-  const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-  const publishableKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
-  return createBrowserClient(url, publishableKey);
-}
